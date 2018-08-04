ef2a8dbfa601cd8da4c39b01546b66ca495f8b3d
monikadembek <monika.dembek@gmail.com>
2018-08-04 12:54:07 +0200

    updated app.js with app configuration and routes

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-var express 	= require("express"),
+let express 	= require("express"),
 	app			= express(),
 	bodyParser 	= require("body-parser"),
 	methodOverride = require("method-override"),
-	mongoose 	= require("mongoose");
+	mongoose 	= require("mongoose"),
+	ejs 		= require("ejs");
 
 //connecting to database
-var dbUrl = process.env.DATABASEURL || "mongodb://localhost:27017/moviesApp";
+let dbUrl = process.env.DATABASEURL || "mongodb://localhost:27017/moviesApp";
 mongoose.connect(dbUrl, {useNewUrlParser: true});
 
 //app configuration
-app.set("port", process.env.PORT || 3300);
-app.use(bodyParser.urlencoded({extended: true})); //used to get post data from forms
-app.set("view engine", "ejs"); //web page templates have ejs extension by default
+app.set("port", process.env.PORT || 3300); //set up port number
+app.use(bodyParser.urlencoded({extended: true})); //used to get POST data from forms
+app.set("view engine", "ejs"); //web page templates have ejs extension by default, we don't need to type it
 app.use(express.static(__dirname + "/public")); //imports static files: stylesheets, js files, from public directory
-app.use(methodOverride("_method")); //allows using put or remove http methods in form which uses post method
+app.use(methodOverride("_method")); //allows using PUT or DELETE http methods in form by overridin POST method
 
 
+//require routes from external files
+const indexRoutes = require("./routes/index");
+const movieRoutes = require("./routes/movie");
+const commentRoutes = require("./routes/comment");
 
-//--------ROUTES-----------------
-app.get("/", function(req, res){
-	res.send("Root route");
-})
+app.use("/", indexRoutes); // enables app to handle requests to "/" routes
+app.use("/movies", movieRoutes); // enables app to handle requests to "/movies" routes
+app.use("/movies/:id/comments", commentRoutes); // enables app to handle requests to "/movies/:id/comments" routes
 
 
-//running node server
+//run node server
 app.listen(app.get("port"), function(){
 	console.log("Server up: http://localhost:" + app.get("port"));
 })
