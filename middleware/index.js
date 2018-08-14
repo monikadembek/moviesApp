d1b927570b343badcfccfc894a19adec41b70bfc
Monika Dembek <monika.dembek@gmail.com>
2018-08-14 16:09:05 +0200

    add authorization for movies function

--- middleware/index.js
+++ middleware/index.js
@@ -3,9 +3,11 @@
 //require necessary models
 const Movie = require("../models/movie");
 const Comment = require("../models/comment");
+const User = require("../models/user");
 
 var middlewareObj = {};
 
+//checks if user is logged in
 middlewareObj.isLoggedIn = function(req, res, next) {
 	if (req.isAuthenticated()) {
 		return next();
@@ -14,12 +16,26 @@ middlewareObj.isLoggedIn = function(req, res, next) {
 	}
 };
 
-
+//checks if user is logged in and if authors id in movie model of selected movie equals user._id
 middlewareObj.checkMovieOwnership = function(req, res, next){
+	//check if user is logged in
 	if (req.isAuthenticated()) {
-		
+		//find movie with specific id
+		Movie.findById(req.params.id, function(err, foundMovie){
+			if (err) {
+				console.log(err);
+				res.redirect("back");
+			} else {
+				//check if movie.author.id = user.id
+				if (foundMovie.author.id.equals(req.user._id)) {
+					next();
+				} else {
+					res.redirect("back");
+				}
+			}
+		});
 	} else {
-		res.redirect("back"); //takes uset back to where they came from
+		res.redirect("/login"); //takes uset back to where they came from
 	}
 }
 
